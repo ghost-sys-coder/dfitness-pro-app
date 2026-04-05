66da6dec281e2216d1e777f4b60b1cc6d5a96378
ghost-sys-coder <juniorbeast177@gmail.com>
2026-04-05 11:01:26 +0300

    Test change

--- app/(onboarding)/fitness-level.tsx
+++ app/(onboarding)/fitness-level.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 
 const FitnessLevel = () => {
     return (
@@ -10,5 +10,3 @@ const FitnessLevel = () => {
 }
 
 export default FitnessLevel
-
-const styles = StyleSheet.create({})
